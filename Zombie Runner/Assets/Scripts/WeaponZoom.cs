9484442d8e17fefacdb713e1abd6df40bfaa799b
avikroytech <68196967+avikroytech@users.noreply.github.com>
2021-12-30 14:51:36 -0800

    Zombie Runner

---
+++ Zombie Runner/Assets/Scripts/WeaponZoom.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class WeaponZoom : MonoBehaviour
+{
+    [SerializeField] Camera FPSCamera;
+    [SerializeField] float zoomedOutFOV = 60f;
+    [SerializeField] float zoomedInFOV = 20f;
+    [SerializeField] float zoomedOutSensitivity = 2f;
+    [SerializeField] float zoomedInSensitivity = 0.5f;
+    [SerializeField] RigidbodyFirstPersonController fpsController;
+
+    bool zoomedInToggle = false;
+
+    void OnDisable() {
+        ZoomOut();
+    }
+    
+    void Update() {
+        if (Input.GetMouseButtonDown(1)) {
+            if (zoomedInToggle == false)
+            {
+                ZoomIn();
+            }
+            else
+            {
+                ZoomOut();
+            }
+        }
+    }
+
+    void ZoomIn()
+    {
+        zoomedInToggle = true;
+        FPSCamera.fieldOfView = zoomedInFOV;
+        fpsController.mouseLook.XSensitivity = zoomedInSensitivity;
+        fpsController.mouseLook.YSensitivity = zoomedInSensitivity;
+    }
+
+    void ZoomOut()
+    {
+        zoomedInToggle = false;
+        FPSCamera.fieldOfView = zoomedOutFOV;
+        fpsController.mouseLook.XSensitivity = zoomedOutSensitivity;
+        fpsController.mouseLook.YSensitivity = zoomedOutSensitivity;
+    }
+}
